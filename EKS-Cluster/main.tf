61abef57fac3745ee7b2b15d6a8b167e711b21ef
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-11 13:16:57 +0530

    Fix typo in subnet data source declaration in main.tf

--- EKS-Cluster/main.tf
+++ EKS-Cluster/main.tf
@@ -2,7 +2,7 @@ data "aws_vpc" "default" { //fetch default subnet
     default = true
 }
 
-data "aws_subnet" "deafult" { //fetch all subnets in the default vpc
+data "aws_subnets" "deafult" { //fetch all subnets in the default vpc
     filter {
         name = "vpc_id" //filter by vpc_id
         values = [data.aws_vpc.default.id] //filter by default vpc id
